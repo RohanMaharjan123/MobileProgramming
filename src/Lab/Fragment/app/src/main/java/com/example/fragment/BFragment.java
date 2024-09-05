58be2f73b5dd3f8186f6806d067f68bbd7867278
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-09-05 17:21:09 +0545

    added a android program that implements fragment UI

---
+++ src/Lab/Fragment/app/src/main/java/com/example/fragment/BFragment.java
@@ -0,0 +1,23 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class BFragment extends Fragment {
+    public BFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_b, container, false);
+        TextView t2 = v.findViewById(R.id.t2); // Fixed ID
+        return v;
+    }
+}
