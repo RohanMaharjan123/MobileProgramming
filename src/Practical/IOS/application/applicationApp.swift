72af3629c9aa906469930a74656395d40d4e4993
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-09-14 14:34:39 +0545

    added swift program that displays hello world and a globe icon

---
+++ src/Practical/IOS/application/applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  applicationApp.swift
+//  application
+//
+//  Created by Rohan Maharjan on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
